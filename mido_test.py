0ca6d2a063efdbe3059c5e0b374a4401c3f180fa
Samuel Colburn <ho0ber@gmail.com>
2021-12-09 20:23:30 -0500

    midio v1

---
+++ mido_test.py
@@ -0,0 +1,35 @@
+import mido
+from dataclasses import dataclass
+
+
+
+@dataclass
+class Column:
+    knob: int
+    fader: int
+
+
+names = mido.get_input_names()
+print(names)
+# # out_port = mido.open_output()
+
+# with mido.open_input(names[0]) as inport:
+#     for msg in inport:
+#         # out_port.send(msg)
+#         print(msg)
+
+columns = [Column(knob=i+80,fader=i+70) for i in range(8)]
+mapping = {}
+for c in columns:
+    mapping[f"control_change:{c.knob}"] = c
+    mapping[f"control_change:{c.fader}"] = c
+    
+dev = "X-Touch-Ext X-TOUCH_INT"
+with mido.open_input(dev) as inport, mido.open_output(dev) as outport:
+    for msg in inport:
+        # import pdb; pdb.set_trace()
+        if msg.type == "control_change":
+            
+            outport.send(mido.Message('control_change', channel=0, control=msg.control-10, value=msg.value))
+        print(msg)
+            
