c9623a5b0e1d7fcdaa77901366c6248b5c3a9ec4
Samuel Colburn <ho0ber@gmail.com>
2021-12-30 15:39:45 -0500

    UI example

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+from PyQt5.QtGui import QGuiApplication
+from PyQt5.QtQml import QQmlApplicationEngine
+
+app = QGuiApplication(sys.argv)
+engine = QQmlApplicationEngine()
+
+engine.quit.connect(app.quit)
+engine.load('./UI/main.qml')
+
+sys.exit(app.exec())
